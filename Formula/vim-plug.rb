55c18714024bfa14f49c69eef79f62a8e639d424
OZAWA Sakuro <sakuro@2238.club>
2019-09-30 11:01:51 +0900

    Remove useless comment

--- Formula/vim-plug.rb
+++ Formula/vim-plug.rb
@@ -4,8 +4,6 @@ class VimPlug < Formula
   url "https://github.com/junegunn/vim-plug/archive/0.10.0.zip"
   sha256 "340fb6c41cfd0e2f2c97af5b1f93c7ba8988f4bb9e94e5ecc14c484dc1fd0364"
 
-  # depends_on "cmake" => :build
-
   def install
     system "install -m 755 -d #{prefix}/share/vim/vimfiles/autoload"
     system "install -m 644 plug.vim #{prefix}/share/vim/vimfiles/autoload/plug.vim"
